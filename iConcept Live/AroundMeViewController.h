afe3b80036bb2570d2baed195337e54df1736083
Jia Zhu <jiazhu@Jias-MacBook-Air.local>
2011-09-18 00:27:27 +1000

    first commit

---
+++ iConcept Live/AroundMeViewController.h
@@ -0,0 +1,13 @@
+//
+//  AroundMeViewController.h
+//  iConcept Live
+//
+//  Created by Jia Zhu on 17/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AroundMeViewController : UIViewController
+
+@end
---
+++ iConcept Live/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  iConcept Live
+//
+//  Created by Jia Zhu on 16/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
